5bee6bee51bd9dc903e71d8630585c5a0398c033
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-03-18 23:11:32 -0400

    Added some fullstack project cards

--- src/components/routes/Fullstack.jsx
+++ src/components/routes/Fullstack.jsx
@@ -1,11 +1,87 @@
 import React, { Component } from 'react'
+import stocplay from '../../media/stocplay.png'
+import rmtradinggame from '../../media/rmtradinggame.png'
+import njtransit from '../../media/njtransit.png'
+import yaywalker from '../../media/yaywalker.png'
 
-export default class Fullstack extends Component {
-    render() {
+export default class Fullstack extends Component
+{
+    render()
+    {
         return (
-            <div>
-                Javascript
-            </div>
+            <React.Fragment>
+                <div className="text-xl">
+                    While attending the GA Software Engineering Immerisive I completed the following projects
+                </div>
+                <div className="flex flex-wrap justify-center">
+
+                    <div className="project-card">
+                        <img className="w-full h-48 object-cover object-left-top" src={yaywalker} alt="Yaywalker" />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">Yay!Walker</div>
+                            <p className="text-gray-700 text-base">
+                                A simple platformer using interfaces. Give it a try <a
+                                    className="font-bold"
+                                    href="https://tsh.itch.io/space-jump-fun">here</a> </p>
+                        </div>
+                        <div className="px-6 py-4">
+                            <span className="hashtag">#rubyonrails</span>
+                            <span className="hashtag">#react</span>
+                            <span className="hashtag">#tailwindcss</span>
+                        </div>
+                    </div>
+
+                    <div className="project-card">
+                        <img className="w-full h-48 object-cover object-left-top" src={njtransit} alt="NJ Transit" />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">NJ Transit</div>
+                            <p className="text-gray-700 text-base">
+                                Some text about this project. And a little but more, check it out <a
+                                    className="font-bold"
+                                    href="https://tsh.itch.io/space-jump-fun">here</a> </p>
+                        </div>
+                        <div className="px-6 py-4">
+                            <span className="hashtag">#express</span>
+                            <span className="hashtag">#sequlize</span>
+                            <span className="hashtag">#react</span>
+                        </div>
+                    </div>
+
+                    <div className="project-card">
+                        <img className="w-full h-48 object-cover object-left-top" src={rmtradinggame} alt="Rick and Morty Trading Game" />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">Rick and Morty Trading Game</div>
+                            <p className="text-gray-700 text-base">
+                                Trade exotic goods while you explore the Rick and Morty Multiverse, try it out<a
+                                    className="font-bold"
+                                    href="https://tsh.itch.io/space-jump-fun">here</a> </p>
+                        </div>
+                        <div className="px-6 py-4">
+                            <span className="hashtag">#react</span>
+                            <span className="hashtag">#rest</span>
+                            <span className="hashtag">#api</span>
+                        </div>
+                    </div>
+
+                    <div className="project-card">
+                        <img className="w-full h-48 object-cover object-left-top" src={stocplay} alt="StocPlay" />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">StocPlay</div>
+                            <p className="text-gray-700 text-base">
+                                Trade virtual stocks to become more confident in the stock market <a
+                                    className="font-bold"
+                                    href="https://tsh.itch.io/space-jump-fun">here</a> </p>
+                        </div>
+                        <div className="px-6 py-4">
+                            <span className="hashtag">#html</span>
+                            <span className="hashtag">#css</span>
+                            <span className="hashtag">#javascript</span>
+                        </div>
+                    </div>
+
+                </div>
+            </React.Fragment>
+
         )
     }
 }
--- src/components/routes/Unity.jsx
+++ src/components/routes/Unity.jsx
@@ -18,7 +18,7 @@ export default class Unity extends Component
                 </div>
                 <div className="flex flex-wrap justify-center">
 
-                    <div className="mt-4 mr-4 max-w-sm rounded overflow-hidden shadow-lg border-gray-400 border">
+                    <div className="project-card">
                         <img className="w-full h-48" src={spacejumpfun} alt="Space Jump Fun" />
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">Space Jump Fun</div>
@@ -28,13 +28,13 @@ export default class Unity extends Component
                                     href="https://tsh.itch.io/space-jump-fun"><span className="text-purple-600 hover:underline">itch.io <i className="fab fa-itch-io text-purple-600"></i></span></a> </p>
                         </div>
                         <div className="px-6 py-4">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#csharp</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#unity</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#interfaces</span>
+                            <span className="hashtag">#csharp</span>
+                            <span className="hashtag">#unity</span>
+                            <span className="hashtag">#interfaces</span>
                         </div>
                     </div>
 
-                    <div className="mt-4 mr-4 max-w-sm rounded overflow-hidden shadow-lg border-gray-400 border">
+                    <div className="project-card">
                         <img className="w-full h-48" src={polymino} alt="Polymino Gardens" />
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">Polymino Gardens</div>
@@ -45,30 +45,30 @@ export default class Unity extends Component
                                     <i className="fab fa-itch-io text-purple-600"></i></span></a> </p>
                         </div>
                         <div className="px-6 py-4">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#csharp</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#unity</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#interfaces</span>
+                            <span className="hashtag">#csharp</span>
+                            <span className="hashtag">#unity</span>
+                            <span className="hashtag">#interfaces</span>
                         </div>
                     </div>
 
-                    <div className="mt-4 mr-4 max-w-sm rounded overflow-hidden shadow-lg border-gray-400 border">
+                    <div className="project-card">
                         <img className="w-full h-48" src={vrprototype} alt="VR Prototype" />
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">VR Prototype</div>
                             <p className="text-gray-700 text-base">
-                                This VR prototype explored various combat mechanics. You can watch a short video of it here on <a
+                                This VR prototype explored various combat mechanics. Watch a gameplay video on <a
                                     className="font-bold"
                                     href="https://tsh.itch.io/space-jump-fun"><span className="text-purple-600 hover:underline">itch.io
                                     <i className="fab fa-itch-io text-purple-600"></i></span></a> </p>
                         </div>
                         <div className="px-6 py-4">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#csharp</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#unity</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#interfaces</span>
+                            <span className="hashtag">#csharp</span>
+                            <span className="hashtag">#unity</span>
+                            <span className="hashtag">#interfaces</span>
                         </div>
                     </div>
 
-                    <div className="mt-4 mr-4 max-w-sm rounded overflow-hidden shadow-lg border-gray-400 border">
+                    <div className="project-card">
                         <img className="w-full h-48" src={witchsword} alt="Witchsword" />
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">Witchsword</div>
@@ -79,9 +79,9 @@ export default class Unity extends Component
                                     <i className="fab fa-itch-io text-purple-600"></i></span></a> </p>
                         </div>
                         <div className="px-6 py-4">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#csharp</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#unity</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#interfaces</span>
+                            <span className="hashtag">#csharp</span>
+                            <span className="hashtag">#unity</span>
+                            <span className="hashtag">#interfaces</span>
                         </div>
                     </div>
 
